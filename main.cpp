515764ae372e2cd8a36ecd1b8919e7b3f7f81278
Ignacio Mercado <igmercado46@gmail.com>
2025-01-18 01:18:43 -0300

    finish getting started/hello window

--- main.cpp
+++ main.cpp
@@ -3,8 +3,14 @@
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-        glViewport(0, 0, width, height);
+    glViewport(0, 0, width, height);
+}
+
+void processInput (GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
     }
+}
 
 int main() {
     if (!glfwInit()) {
@@ -42,6 +48,15 @@ int main() {
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     while (!glfwWindowShouldClose(window)) {
+        /*inputs*/
+        processInput(window);
+        /********/
+
+        /*rendering*/
+        glClearColor(0.0f, 0.2f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        /***********/
+        
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
