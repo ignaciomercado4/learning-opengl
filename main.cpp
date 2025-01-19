de13ebc7b447b7d93b982bad0f4f12c736eef5b1
Ignacio Mercado <igmercado46@gmail.com>
2025-01-18 23:56:14 -0300

    finished first exercise

--- main.cpp
+++ main.cpp
@@ -2,6 +2,11 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+/*constants*/
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+/***********/
+
 /*function declarations*/
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -66,30 +71,24 @@ int main()
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     float vertices[] = {
-        0.5f,  0.5f, 0.0f,  // top right
-        0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left 
-    };
-    unsigned int indices[] = {  // note that we start from 0!
-        0, 1, 3,   // first triangle
-        1, 2, 3    // second triangle
+         0.0f, -0.5f, 0.0f,
+         1.0f, -0.5f, 0.0f,
+         0.5f,  0.5f, 0.0f,
+         -1.0f, -0.5f, 0.0f,
+         0.0f, -0.5f, 0.0f,
+         -0.5f, 0.5f, 0.0f
     };  
 
-    unsigned int VAO, VBO, EBO;
+    unsigned int VAO, VBO;
     glGenVertexArrays(1, &VAO); 
     glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
+
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);  
-
-
+    glEnableVertexAttribArray(0);
 
     unsigned int vertexShader;
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -127,9 +126,6 @@ int main()
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    // draw only wireframe
-    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); 
-
     while (!glfwWindowShouldClose(window))
     {
         /*inputs*/
@@ -140,11 +136,10 @@ int main()
         glClearColor(0.0f, 0.2f, 1.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // draws rectangle
+        // draws triangle
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        glBindVertexArray(0);
+        glDrawArrays(GL_TRIANGLES, 0, sizeof(vertices));
 
         glfwSwapBuffers(window);
         glfwPollEvents();
