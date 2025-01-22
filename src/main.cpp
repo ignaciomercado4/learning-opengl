8f354a230eabac2503d241ca2196a13c96d0763e
Ignacio Mercado <igmercado46@gmail.com>
2025-01-22 01:00:31 -0300

    now derp isnt upside down

--- src/main.cpp
+++ src/main.cpp
@@ -100,6 +100,7 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     int width, height, nrChannels;
+    stbi_set_flip_vertically_on_load(true);  
     unsigned char *data = stbi_load("src/resources/container.jpg", &width, &height, &nrChannels, 0);
     if (data) {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
